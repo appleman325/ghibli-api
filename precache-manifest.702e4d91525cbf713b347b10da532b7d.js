a1f0f582819bbb0b93f561c4110763700dabaa47
Yixin <yixin0325@gmail.com>
2020-04-23 22:03:49 -0400

    Updates

---
+++ precache-manifest.702e4d91525cbf713b347b10da532b7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghibli-api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d0769cbcdb8c0bbb0d11b802d8e0f9a",
+    "url": "/ghibli-api/static/media/Studio_Ghibli_logo.svg.2d0769cb.png"
+  },
+  {
+    "revision": "6864b1de86f8e5d1638e",
+    "url": "/ghibli-api/static/js/runtime~main.6864b1de.js"
+  },
+  {
+    "revision": "74512a0662f90b4ce590",
+    "url": "/ghibli-api/static/js/main.74512a06.chunk.js"
+  },
+  {
+    "revision": "98cf5165e1dfd9d163df",
+    "url": "/ghibli-api/static/js/1.98cf5165.chunk.js"
+  },
+  {
+    "revision": "74512a0662f90b4ce590",
+    "url": "/ghibli-api/static/css/main.c48654b5.chunk.css"
+  },
+  {
+    "revision": "98cf5165e1dfd9d163df",
+    "url": "/ghibli-api/static/css/1.29b27f2b.chunk.css"
+  },
+  {
+    "revision": "bee451e0a61d8f21ad6821e06285c3c5",
+    "url": "/ghibli-api/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ghibli-api/precache-manifest.702e4d91525cbf713b347b10da532b7d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ghibli-api/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
